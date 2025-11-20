9b358529605cb5557c47fcaefb0511b8bc38a281
m-221 <24308060610066@cetis61.edi.mx>
2025-11-20 06:42:33 -0700

    meleynuevo2

--- app.py
+++ app.py
@@ -1,48 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, jsonify, request,url_for,redirect,render_template
 import requests
-
 app = Flask(__name__)
-app.secret_key = 'melyyyyaaasdwwd'
+app.secret_key = 'tumelyKEY1904'
 
-API_URL = 'https://api.spoonacular.com/recipes/findByIngredients'
-API_KEY = '923b514b2c604404954302eaebfea6fd'
 
 @app.route('/')
 def index():
-    return render_template('inicio.html')
+    return render_template('index.html')
 
-@app.route('/resetas')
-def rs():
-    return render_template('reseta.html')
-
-@app.route('/buscar', methods=['GET', 'POST'])
+@app.route('/recetas')
 def buscar_recetas():
-    recetas = []
-    
-    if request.method == 'POST':
-        ingrediente = request.form.get('ingrediente')
-        if ingrediente:
-            params = {
-                'apiKey': API_KEY,
-                'ingredients': ingrediente, 
-                'number': 10,                 
-                'ranking': 1            
-            }
+    return  render_template('recetas.html')
 
-            respuesta = requests.get(API_URL, params=params)
-            
-        
-            if respuesta.status_code == 200:
-                recetas = respuesta.json()
+@app.route('/buscar')
+def buscar():
+     return
 
-            else:
-                flash('Error al obtener las recetas. Intenta de nuevo más tarde.', 'error')
-                return render_template('inicio.html')
-        else:
-            flash('Por favor, ingresa un ingrediente para buscar.', 'error')
-            return render_template('inicio.html')
 
-    return render_template('receta.html', recetas=recetas)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+
+
+if __name__  == '__main__':
+       app.run(debug=True)
